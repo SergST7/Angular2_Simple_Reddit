46d496f7566cea448a17dcc91431545cee7db73b
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-02-15 14:23:05 +0200

    Add submit button

--- app.ts
+++ app.ts
@@ -14,16 +14,26 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
       <h1 class="ui header">Add a Link</h1>
       <div  class="field">
         <label for="title">Title: </label>
-        <input type="text" name="title">
+        <input type="text" name="title" #newTitle>
       </div>
       <div  class="field">
         <label for="link">Link: </label>
-        <input type="text" name="link">
+        <input type="text" name="link" #newLink>
       </div>
+      
+      <button class="ui button positive right floated"
+        (click)="addArticle(newTitle, newLink)">
+        Submit add link
+      </button>
     </form>`
 })
 
-class RedditApp{}
+class RedditApp{
+    addArticle(title: HTMLInputElement, link:HTMLInputElement): boolean{
+        console.log(`Adding title ${title.value} and link ${link.value}`);
+        return false
+    }
+}
 
 
 @NgModule({
